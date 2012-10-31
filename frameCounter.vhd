1c4fb66ef364c4568d3d21f1f4c3f309452ae772
Yang Su <suyang1+services@gmail.com>
2012-10-31 19:29:45 -0400

    Implemented frameCounter

--- frameCounter.vhd
+++ frameCounter.vhd
@@ -11,8 +11,29 @@ ENTITY frameCounter IS
 	);
 END frameCounter;
 
-ARCHITECTURE counter OF frameCounter IS
+ARCHITECTURE arch OF frameCounter IS
+
+	COMPONENT counter
+	PORT
+	(
+		aclr		: IN STD_LOGIC ;
+		clk_en		: IN STD_LOGIC ;
+		clock		: IN STD_LOGIC ;
+		cnt_en		: IN STD_LOGIC ;
+		data		: IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+		sload		: IN STD_LOGIC ;
+		q		: OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
+	);
+	END COMPONENT;
 
 BEGIN
-
-END counter;
+	counter0: counter PORT MAP(
+		aclr 	=> 	aclr,
+		clk_en	=> 	'1',
+		clock	=>	clk,
+		cnt_en	=>	enable,
+		data	=> "000000000000",
+		sload	=> '0',
+		q		=> count
+	);
+END arch;
